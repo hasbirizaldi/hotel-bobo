3bbdd4d2695c5cc33d0f644ff73b99d3b98b539b
hasbirizaldi <hasbirizaldi5@gmail.com>
2024-09-08 02:22:28 +0700

    feed:tiket kereta

---
+++ app/Http/Controllers/LuxeryTrainController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LuxeryTrain;
+use Illuminate\Http\Request;
+
+class LuxeryTrainController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LuxeryTrain $luxeryTrain)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LuxeryTrain $luxeryTrain)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LuxeryTrain $luxeryTrain)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LuxeryTrain $luxeryTrain)
+    {
+        //
+    }
+}
